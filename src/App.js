f1108ba307523c5813144ad29e9df34dd9b12921
ngimdock <ngimdockzemfack@gmail.com>
2021-10-28 09:01:12 +0100

    test connnexion github vscode

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div className="text-center text-green-500">
-           Burger house
+           Burger house modification
         </div>
     );
 };
