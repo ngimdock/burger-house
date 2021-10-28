7b3540911bfffa63a7090923d812a03c5b323572
ngimdock <ngimdockzemfack@gmail.com>
2021-10-28 08:53:20 +0100

    init project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/index.css';   // import du css de tailwind
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
