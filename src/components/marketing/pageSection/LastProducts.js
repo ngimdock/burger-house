10d6805d3f0975721856049f07c71efb230d7d95
ngimdock <ngimdockzemfack@gmail.com>
2021-11-21 22:23:41 +0100

    gestion responsive de la section last products

--- src/components/marketing/pageSection/LastProducts.js
+++ src/components/marketing/pageSection/LastProducts.js
@@ -8,33 +8,33 @@ const LastProducts = () => {
 
 	return(
 		<Container>
-			<div className="grid grid-cols-2 grid-rows-2 gap-x-9 gap-y-9 w-full h-96 px-11 my-20 text-white uppercase">
+			<div className="grid md:grid-cols-2 grid-rows-3  md:grid-rows-2 gap-y-5 md:gap-3 lg:gap-9 w-full h-96 px-0 lg:px-11 my-20 text-white uppercase">
 
-				<div className="relative row-span-2 bg-primary h-full rounded-md p-6">
-					<img src={imgProduct1} className="absolute top-0 left-0 z-1 rounded-md w-full h-full object-cover" alt="Un delicieux hamburger pose sur un bois" />
-					{/*<div className="absolute top-0 left-0 z-3 w-full h-full bg-gradient-to-r from-yellow-400 via-yellow-600 to-yellow-700 opacity-10"></div>*/}
-					<div className="relative z-3">
+				<div className="relative md:row-span-2 bg-primary h-full rounded-md p-6">
+					<img src={imgProduct1} className="absolute top-0 left-0 z-0 rounded-md w-full h-full object-cover" alt="Un delicieux hamburger pose sur un bois" />
+					<div className="block sm:hidden rounded-md absolute top-0 left-0 z-10 w-full h-full bg-black opacity-50"></div>
+					<div className="relative z-20">
 						<span className="block font-bold text-lg font-sans">killer burger</span>
-						<span className="text-2xl font-black font-sans">burger le plus populaire</span>
+						<span className="text-xl lg:text-2xl font-black font-sans">burger le plus populaire</span>
 					</div>
 				</div>
 
 				<div className="relative bg-primary h-full rounded-md p-6">
-					<img src={imgProduct2} className="absolute top-0 left-0 z-1 rounded-md w-full h-full object-cover" alt="deux delicieux hamburger pose sur un bois" />
-					{/*<div className="absolute top-0 left-0 z-3 w-full h-full bg-gradient-to-r from-yellow-400 via-yellow-600 to-yellow-700 opacity-10"></div>*/}
-					<div className="relative z-3">
+					<img src={imgProduct2} className="absolute top-0 left-0 z-0 rounded-md w-full h-full object-cover" alt="deux delicieux hamburger pose sur un bois" />
+					<div className="block sm:hidden rounded-md absolute top-0 left-0 z-10 w-full h-full bg-black opacity-50"></div>
+					<div className="relative z-20">
 						<span className="block font-bold text-lg font-sans">Island burger</span>
-						<span className="block text-2xl font-black font-sans">Plus de plaisir</span>
-						<span className="text-2xl font-black font-sans">plus de gout</span>
+						<span className="block text-xl lg:text-2xl font-black font-sans">Plus de plaisir</span>
+						<span className="text-xl lg:text-2xl font-black font-sans">plus de gout</span>
 					</div>
 				</div>
 
 				<div className="relative bg-primary h-full rounded-md p-6">
-					<img src={imgProduct3} className="absolute top-0 left-0 z-1 rounded-md w-full h-full object-cover" alt="Les elements de fabrication d'un hamburger" />
-					{/*<div className="absolute top-0 left-0 z-3 w-full h-full bg-gradient-to-r from-yellow-400 via-yellow-600 to-yellow-700 opacity-10"></div>*/}
-					<div className="relative z-3">
+					<img src={imgProduct3} className="absolute top-0 left-0 z-0 rounded-md w-full h-full object-cover" alt="Les elements de fabrication d'un hamburger" />
+					<div className="block sm:hidden rounded-md absolute top-0 left-0 z-10 w-full h-full bg-black opacity-50"></div>
+					<div className="relative z-20">
 						<span className="block font-bold text-lg font-sans">Orlando's burger</span>
-						<span className="text-2xl font-black font-sans">frais & pimente</span>
+						<span className="text-xl lg:text-2xl font-black font-sans">frais & pimente</span>
 					</div>
 				</div>
 
